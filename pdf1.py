2bbfa7983ac3789a5b3cfb643744e9a11823a49c
MyShoesHaveSouls <99381059+MyShoesHaveSouls@users.noreply.github.com>
2024-08-30 18:42:04 -0600

    Update pdf1.py

--- pdf1.py
+++ pdf1.py
@@ -1,7 +1,7 @@
 import PyPDF2
 
 # Open the PDF file
-with open('yourfile.pdf', 'rb') as file:
+with open('cctobtc.pdf', 'rb') as file:
     reader = PyPDF2.PdfReader(file)
     for page_num in range(len(reader.pages)):
         page = reader.pages[page_num]
